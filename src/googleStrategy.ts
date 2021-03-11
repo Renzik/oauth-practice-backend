0ef1e3750024c2ed1a7674caccb0c241ed77fd56
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2021-03-11 13:31:32 -0400

    testing redirect to /

--- src/googleStrategy.ts
+++ src/googleStrategy.ts
@@ -46,6 +46,9 @@ router.get(
   '/auth/google/callback',
   passport.authenticate('google', {
     failureRedirect: '/login',
-    successRedirect: '/',
+    successRedirect:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000'
+        : 'https://modest-einstein-76cd0d.netlify.app/',
   })
 );
